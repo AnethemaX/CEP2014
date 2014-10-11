f6dfa8cbf9b6ea470ebf1f8bc94999c64eabacbe
bryanyjy <kids@Jenny-Lis-MacBook-Pro-2.local>
2014-10-11 17:55:49 +0800

    js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
 		var divposition = $(location.href.substring(location.href.lastIndexOf("#"))).offset();
 		$("html, body").animate({scrollTop: divposition.top}, "slow");
 	}
+	$("a[href*='#']").click(function(){
+		var divposition = $(this.href.substring(this.href.lastIndexOf("#"))).offset();
+		$("html, body").animate({scrollTop: divposition.top-60}, "slow");
+	});
 });
